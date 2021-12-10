018d335deb2f0a38c258d452cb9318b18b6885d0
David Shuler <davi8119@mdr65qmd6r.fritz.box>
2021-12-10 19:02:18 +0100

    first commit of node.js api

---
+++ index.js
@@ -0,0 +1,124 @@
+const PORT = 8080
+const express = require('express')
+const axios = require('axios').default
+const cheerio = require('cheerio')
+
+// This just lets us call express commands with app.<thing>()
+const app = express()
+
+// Create an array of the different sites we want to scrape links from
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        // The telegraph uses relative hrefs on their site so we need a baseurl
+        // ? How can this be written to just dynamically add a baseurl when needed?
+        base: 'https://www.telegraph.co.uk'
+    }
+]
+
+// Create an array to put all the article links you return later.
+const articles = []
+
+// Write a function to collect links rather then in our app.get routing
+newspapers.forEach(newspaper => {
+    // Go make a request to the URL
+    axios.get(newspaper.address, )
+        // We are able to "then" the function to do stuff on the response.
+        .then(response => {
+            const html = response.data
+            // I sort of get how we're using $, but I don't REALLY understand it is or does.
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function(){
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                // Push specific info as an object in to the artciles array we created above.
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+        })
+})
+
+// Here is where we handle request to the root page
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climat Change News API')
+})
+
+// This is the /news page that scrapes the guardian for URLs that have "climate" in the title
+app.get('/news', (req, res) => {
+    // axios.get('https://www.theguardian.com/environment/climate-crisis') 
+    //     .then((response) => {
+    //         const html = response.data
+    //         const $ = cheerio.load(html)
+
+    //         $('a:contains("climate")', html).each(function() {
+    //             const title = $(this).text()
+    //             const url = $(this).attr('href')
+    //             articles.push({
+    //                 title,
+    //                 url
+    //             })
+    //         })
+    //         res.json(articles)
+    //     }).catch((err) => console.log(err))
+
+    // Just respond with the ~list~ array of articles we created above
+    res.json(articles)
+
+    // Some of the articles titles contain escape characters.  Could we write a regex to scrape these out without "hurting" the URL or title?
+})
+
+// you can name newspaperId whatever you want.  the : tells nodejs that whatever comes next is going to be a parameter.
+app.get('/news/:newspaperId', async (req, res) => {
+    // This will return EVERYTHING in the request
+    // console.log(req)
+    // This will return on the the parameter you select (as long as it exists)
+    // console.log(req.params.newspaperId)
+    const newspaperId = req.params.newspaperId
+
+    // filter the newspapers array above and look for something that matches :newspaperId and return the "address" parameter of the first found object.
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    // Also defining a baseURL that we can later, but it has to be pulled from array as well.
+    // (there's gotta be a cleaner way of doing both of these things)
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+    // Do another axios get and "then" chain
+    axios.get(newspaperAddress)
+        // I'm not sure what the => {} means, but it seems to be working.
+        // Note:  This is variable declaration.  Make sure you spell "response" right.
+        .then(response => {
+            // SOOOO many local variables with the same name...
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
